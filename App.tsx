878205be80259c4df6f8f89339b49909217d8fe7
David Gomez <davidgomez@Mac-mini-de-David.local>
2022-11-27 10:17:33 -0500

    hola mundo react native

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
+
+const App = () => {
+  return (
+    <HolaMundoScreen/>
+  )
+}
+
+export default App;
